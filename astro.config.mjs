a92255324804d57cb212d2410dfa48c146525c83
Gabriel Nascimento <gabrielnpoa@gmail.com>
2023-02-08 14:33:54 -0300

    add SEO schema and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -10,8 +10,12 @@ import image from "@astrojs/image";
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
+import robotsTxt from "astro-robots-txt";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  site: "https://gabrielnascimento.netlify.app",
+  integrations: [tailwind(), image(), robotsTxt()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
