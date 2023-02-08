e6c35f2b05dbdc2eac2f882e9a74089490c1855b
Gabriel Nascimento <gabrielnpoa@gmail.com>
2023-02-08 13:31:11 -0300

    basic estructure fineshed

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), image()],
+  output: "server",
+  adapter: netlify()
+});
